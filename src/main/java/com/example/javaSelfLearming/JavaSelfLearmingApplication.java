df7c4adade589796f9c6c3cbaa3a6b07a7311100
KimBYH <alxnfkfkfk@naver.com>
2025-08-10 07:09:15 +0900

    test: 프로젝트 생성 및 깃허브 원격저장소 연결 확인

---
+++ src/main/java/com/example/javaSelfLearming/JavaSelfLearmingApplication.java
@@ -0,0 +1,13 @@
+package com.example.javaSelfLearming;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaSelfLearmingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaSelfLearmingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/javaSelfLearming/JavaSelfLearmingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.javaSelfLearming;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaSelfLearmingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
