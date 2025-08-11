6d2ccd53cf7085dd325842ae944c66e519cc3c7d
KBH <alxnfkfkfk@naver.com>
2025-08-11 14:57:46 +0900

    1week: 2장 스프링 부트 시작하기 완료

--- src/main/java/com/example/javaSelfLearming/controller/TestController.java
+++ src/main/java/com/example/javaSelfLearming/controller/TestController.java
@@ -1,13 +1,17 @@
 package com.example.javaSelfLearming.controller;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class TestController {
 
     @GetMapping("/")
     public String test(){
         return "hello";
+    }
+    @GetMapping("/hello")
+    public String hello(){
+        return "helloworld";
     }
 }
