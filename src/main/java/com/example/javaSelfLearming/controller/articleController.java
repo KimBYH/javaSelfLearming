79d8e089307cc6600e810874ba6243fe7c4c06e9
KBH <alxnfkfkfk@naver.com>
2025-08-13 16:37:46 +0900

    feat: 1week 5장 게시글 읽기 Article 완료

--- src/main/java/com/example/javaSelfLearming/controller/articleController.java
+++ src/main/java/com/example/javaSelfLearming/controller/articleController.java
@@ -6,9 +6,12 @@ import com.example.javaSelfLearming.repository.ArticleRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import java.util.ArrayList;
 
 @Slf4j
 @Controller
@@ -18,9 +21,27 @@ public class articleController {
     @Autowired // 스프링에서 해당 객체를 주입시켜줌 (의존성 주입 DI : Dependency Injection)
     private ArticleRepository articleRepository;
 
+    @GetMapping("")
+    public String index(Model model){
+        ArrayList<Article> articlesEntityList = articleRepository.findAll();
+
+        model.addAttribute("articlesList", articlesEntityList);
+        return "articles/index";
+    }
+
     @GetMapping("/new")
     public String newArticleForm(){
         return "articles/new";
+    }
+
+    @GetMapping("/{id}")
+    public String show(@PathVariable Long id, Model model) {
+
+        Article articleEntity = articleRepository.findById(id).orElse(null);
+
+        model.addAttribute("article", articleEntity);
+
+        return "articles/show";
     }
 
     @PostMapping("/create")
